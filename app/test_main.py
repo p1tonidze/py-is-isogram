99dc8303c3db5e479cdc49aced917794267e48d2
p1tonidze <p1t0nidze422@gmail.com>
2024-09-25 13:51:38 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,14 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
+    assert is_isogram("") is True
+    assert is_isogram("abcdefg") is True
+    assert is_isogram("AaBbCc") is False
+    assert is_isogram("unique") is False
+    assert is_isogram("nonrepeat") is False
+    assert is_isogram("12345") is True
+    assert is_isogram("") is True
